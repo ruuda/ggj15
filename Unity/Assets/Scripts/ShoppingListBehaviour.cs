3a48105f692ce7ff1e6286ef84a674ef1faca75d
Ruud van Asseldonk <dev@veniogames.com>
2015-01-23 23:20:28 +0100

    Add shopping list

---
+++ Unity/Assets/Scripts/ShoppingListBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ShoppingListBehaviour : MonoBehaviour {
+
+	public int listLength = 5;
+
+	private Queue<Collectable> collectables = new Queue<Collectable>();
+
+	void Awake () {
+		var available = ((Collectable[])System.Enum.GetValues(typeof(Collectable))).ToList ();
+		available.Shuffle();
+		for (int i = 0; i < listLength; i++) {
+			collectables.Enqueue(available[i]);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public enum Collectable {
+	Banana,
+	Strawberry,
+	Crisps,
+	Milk,
+	Potato,
+	Kiwi,
+	Lemon
+}
+
+static class ListExtensions {
+	public static void Shuffle<T> (this IList<T> list) {  
+		var rng = new System.Random();
+		int n = list.Count;
+		while (n > 1) {
+			n--;
+			int k = rng.Next(n + 1);
+			T value = list[k];
+			list[k] = list[n];
+			list[n] = value;
+		}
+	}
+}
