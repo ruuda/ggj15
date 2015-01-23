b6823e864f2ae9846cce5cd507389bc0514d9f1e
Ruud van Asseldonk <dev@veniogames.com>
2015-01-23 22:29:44 +0100

    Add forward stepping controls

--- Unity/Assets/Scripts/PlayerBehaviour.cs
+++ Unity/Assets/Scripts/PlayerBehaviour.cs
@@ -1,35 +1,83 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerBehaviour : MonoBehaviour {
 
 	public float moveSpeed = 5.0f;
 
-	// Use this for initialization
+	private Queue<Movement> movements = new Queue<Movement>();
+	private float movementT;
+
+	private Vector3 waistPosition { get { return this.transform.position + new Vector3(0f, 0.5f, 0f); } }
+
+
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		var camPos = Camera.main.transform.position;
-		var camToMouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f)) - camPos;
-		var t = - camPos.y / camToMouse.y;
-		var planePos = camPos + t * camToMouse;
-		var thisToMouse = planePos.ProjectY() - transform.position.ProjectY();
-		transform.rotation = Quaternion.FromToRotation(Vector3.forward, thisToMouse);
 
-		if (Input.GetKey(KeyCode.W))
+	void Update () {
+		if (movements.Count == 0) {
+			HandleInput();
+		} else {
+			HandleMove();
+		}
+	}
+
+	private void HandleInput () {
+		var left = transform.TransformDirection(Vector3.left);
+		var right = transform.TransformDirection(Vector3.right);
+		var forward = transform.forward;
+		
+		var canLeft = !Physics.Raycast(waistPosition, left, 0.6f);
+		var canRight = !Physics.Raycast(waistPosition, right, 0.6f);
+		var canForward = !Physics.Raycast(waistPosition, forward, 0.6f);
+		
+		Debug.DrawRay(waistPosition, left, Color.red, 0f);
+		Debug.DrawRay(waistPosition, right, Color.blue, 0f);
+		Debug.DrawRay(waistPosition, forward, Color.cyan, 0f);
+
+		Debug.Log (canForward.ToString());
+
+		if (canForward && Input.GetKey(KeyCode.W))
 		{
-			transform.position += (transform.rotation * Vector3.forward) * Time.deltaTime * moveSpeed;
+			var move = new Movement { kind = MovementKind.Move, fromPos = this.transform.position, toPos = this.transform.position + forward };
+			this.movements.Enqueue(move);
+		}
+	}
+
+	private void HandleMove () {
+
+		var movement = this.movements.Peek();
+		this.movementT += Time.deltaTime;
+		var t = this.movementT;
+
+		// This one is done now.
+		if (t > 1.0f)
+		{
+			this.movementT = 0.0f;
+			t = 1.0f;
+			this.movements.Dequeue();
+		}
+
+		// Ease / smooph step.
+		t = Mathf.SmoothStep(0.0f, 1.0f, t);
+
+		if (movement.kind == MovementKind.Move)
+		{
+			this.transform.position = movement.fromPos * (1.0f - t) + movement.toPos * t;
 		}
 	}
 }
 
-static class VecUtils
-{
-	public static Vector3 ProjectY(this Vector3 vec)
-	{
-		return new Vector3(vec.x, 0.0f, vec.z);
-	}
+internal enum MovementKind {
+	Rotate,
+	Move
+}
+
+internal struct Movement {
+	public MovementKind kind;
+	public Vector3 fromPos;
+	public Vector3 toPos;
+	public Quaternion fromRot;
+	public Quaternion toRot;
 }
