bf67c143c55311cb68a900e17addab8bfb34c717
Ruud van Asseldonk <dev@veniogames.com>
2015-01-24 01:42:26 +0100

    Add simple child that follows parent

---
+++ Unity/Assets/Scripts/ChildBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildBehaviour : MonoBehaviour {
+
+	private bool isFollowing = true;
+	private GameObject player;
+	private Vector3 initialOffset;
+	private Quaternion initialRotation;
+
+	// Use this for initialization
+	void Start () {
+		this.player = GameObject.FindGameObjectWithTag("Player");
+		initialOffset = transform.position - player.transform.position;
+		initialRotation = transform.rotation * Quaternion.Inverse(player.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isFollowing) {
+			this.transform.position = player.transform.position + initialOffset;
+			this.transform.rotation = player.transform.rotation * initialRotation;
+		}
+	}
+}
