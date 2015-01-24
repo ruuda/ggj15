5fb19ee692e0746de02010cff2aa557e7a9d262e
Ruud van Asseldonk <dev@veniogames.com>
2015-01-24 10:34:18 +0100

    Generate UI for shopping list (no removal yet)

--- Unity/Assets/Scripts/ShoppingListBehaviour.cs
+++ Unity/Assets/Scripts/ShoppingListBehaviour.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UI;
 
 public class ShoppingListBehaviour : MonoBehaviour {
 
 	public int listLength = 5;
+	public float listUIX = 0.7f;
+	public float listUIY = 0.4f;
+	public float listUIWidth = 0.25f;
+
+	public Sprite listTopSprite;
+	public Sprite bananasSprite;
+	public Sprite strawberriesStprite;
+	public Sprite crispsSprite;
+	public Sprite milkSprite;
+	public Sprite potatoesSprite;
+	public Sprite kiwisSprite;
+	public Sprite lemonsSprite;
+	public Sprite listBottomSprite;
 
 	private List<Collectable> collectables = new List<Collectable>();
-	private ShoppingListUI shoppingListUI;
+
+	private Canvas uiCanvas;
 
 	void Awake () {
 		// Get a random subset of the available items of the desired length.
@@ -16,13 +31,51 @@ public class ShoppingListBehaviour : MonoBehaviour {
 		for (int i = 0; i < listLength; i++) {
 			collectables.Add(available[i]);
 		}
+	}
 
-		shoppingListUI = GameObject.FindGameObjectWithTag("ShoppingListText").GetComponent<ShoppingListUI>();
+	private Sprite CollectableToSprite (Collectable collectable) {
+		switch (collectable) {
+		case Collectable.Bananas: return bananasSprite;
+		case Collectable.Strawberries: return strawberriesStprite;
+		case Collectable.Crisps: return crispsSprite;
+		case Collectable.Kiwis: return kiwisSprite;
+		case Collectable.Lemons: return lemonsSprite;
+		case Collectable.Milk: return milkSprite;
+		case Collectable.Potatoes: return potatoesSprite;
+		}
+		throw new System.ArgumentException("collectable");
+	}
+
+	void CreateUI () {
+		var canvasObject = new GameObject("Canvas");
+		canvasObject.layer = LayerMask.NameToLayer("UI");
+		uiCanvas = canvasObject.AddComponent<Canvas>();
+		uiCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+		uiCanvas.pixelPerfect = true;
+
+		var sprites = new List<Sprite>();
+		sprites.Add(listTopSprite);
+		sprites.AddRange(collectables.Select(c => CollectableToSprite(c)));
+		sprites.Add(listBottomSprite);
+
+		var origin = new Vector2(listUIX, listUIY);
+		foreach (var sprite in sprites) {
+			var spriteObject = new GameObject("Image" + sprite.name);
+			spriteObject.transform.parent = canvasObject.transform;
+			var image = spriteObject.AddComponent<Image>();
+			image.sprite = sprite;
+			var uiHeight = sprite.bounds.size.y / sprite.bounds.size.x * listUIWidth * Camera.main.aspect;
+			image.rectTransform.anchorMin = origin - new Vector2(0.0f, uiHeight);
+			image.rectTransform.anchorMax = origin + new Vector2(listUIWidth, 0.0f);
+			image.rectTransform.anchoredPosition = new Vector2(0.0f, 0.0f);
+			image.rectTransform.sizeDelta = new Vector2(1.0f, 1.0f);
+			origin -= new Vector2(0.0f, uiHeight);
+		}		
 	}
 
 	// Use this for initialization
 	void Start () {
-		shoppingListUI.SetList(this.collectables);
+		CreateUI();
 	}
 	
 	// Update is called once per frame
@@ -34,7 +87,7 @@ public class ShoppingListBehaviour : MonoBehaviour {
 		if (this.collectables.Remove(collectable)) {
 			Debug.Log(string.Format ("{0} collected", collectable));
 			// TODO: Special effects, whatnot.
-			shoppingListUI.SetList(this.collectables);
+			// TODO: Update list appearance.
 		} else {
 			Debug.Log(string.Format("{0} not collected, not on list", collectable));
 		}
@@ -42,13 +95,13 @@ public class ShoppingListBehaviour : MonoBehaviour {
 }
 
 public enum Collectable {
-	Banana,
-	Strawberry,
+	Bananas,
+	Strawberries,
 	Crisps,
 	Milk,
-	Potato,
-	Kiwi,
-	Lemon
+	Potatoes,
+	Kiwis,
+	Lemons
 }
 
 static class ListExtensions {
