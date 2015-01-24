1749bddf998cb2bee9c561173422dc0baa921503
Ruud van Asseldonk <dev@veniogames.com>
2015-01-24 21:10:46 +0100

    Add pointer to point at collectables

---
+++ Unity/Assets/Scripts/PointerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerBehaviour : MonoBehaviour {
+
+	private float angle = 0.0f;
+	private Vector3 initialPosition;
+
+	public float amplitude = 0.1f;
+	public float rotationSpeed = 40.0f;
+	public float hoverSpeed = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angle += Time.deltaTime * hoverSpeed;
+
+		this.transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+		this.transform.position = initialPosition + new Vector3(0.0f, Mathf.Cos(angle) * amplitude);
+
+		// TODO: Adjust height based on distance to player, with a maximum.
+		// (Move down when a goal is near).
+	}
+}
